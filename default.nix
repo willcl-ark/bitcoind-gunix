152c9de3a8977f1096b84b278a0b208fbaf1e229
will <will@256k1.dev>
2025-05-20 22:06:19 +0100

    update bitcoind commit

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  version = "ec5d1c372b20d49147813aa0392195a3642b86a1";
+  version = "9a887baadebce7c2e76df831fad54c5fa81d309e";
   url = "https://github.com/bitcoin/bitcoin/archive/${version}.tar.gz";
-  sha256 = "sha256-TFtRwFmLK1Q7HrSJa4StLASNgd37cbxqz2HTzqojvtY=";
+  sha256 = "sha256-mbXamzOOXrL6AVc3wgt93KfijFghX/DMLyHjusuMcNQ=";
 
   depends = pkgs.callPackage ./depends.nix { inherit version url sha256; };
   bitcoind = pkgs.callPackage ./bitcoind.nix { inherit url sha256 depends; };
