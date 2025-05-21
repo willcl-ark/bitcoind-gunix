d4fcabcc41bc3c19af7b760e40a7e17c5cec2f5f
will <will@256k1.dev>
2025-05-21 10:29:42 +0100

    fixup! update depends packages

--- depends.nix
+++ depends.nix
@@ -132,19 +132,19 @@ let
     qt-top-cmakelists = {
       urlPrefix = qt_top_prefix;
       file = "CMakeLists.txt?h=${qt_version}";
-      extractedFile = "CMakeLists.txt";
+      extractedFile = "CMakeLists.txt-${qt_version}";
       sha256 = "9fb720a633c0c0a21c31fe62a34bf617726fed72480d4064f29ca5d6973d513f";
     };
     qt-top-cmake-ecmoptional = {
       urlPrefix = "${qt_top_prefix}/cmake";
       file = "ECMOptionalAddSubdirectory.cmake?h=${qt_version}";
-      extractedFile = "ECMOptionalAddSubdirectory.cmake";
+      extractedFile = "ECMOptionalAddSubdirectory.cmake-${qt_version}";
       sha256 = "97ee8bbfcb0a4bdcc6c1af77e467a1da0c5b386c42be2aa97d840247af5f6f70";
     };
     qt-top-cmake-helpers = {
       urlPrefix = "${qt_top_prefix}/cmake";
       file = "QtTopLevelHelpers.cmake?h=${qt_version}";
-      extractedFile = "QtTopLevelHelpers.cmake";
+      extractedFile = "QtTopLevelHelpers.cmake-${qt_version}";
       sha256 = "5ac2a7159ee27b5b86d26ecff44922e7b8f319aa847b7b5766dc17932fd4a294";
     };
     qt-translations = {
